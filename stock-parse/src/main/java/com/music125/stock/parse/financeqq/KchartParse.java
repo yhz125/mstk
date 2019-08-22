5e6993ac0ab3c41140998478298c9acf1da42aba
yinhz <yinhz@jinxiang.com>
2019-08-22 16:43:36 +0800

    fix

--- stock-parse/src/main/java/com/music125/stock/parse/financeqq/KchartParse.java
+++ stock-parse/src/main/java/com/music125/stock/parse/financeqq/KchartParse.java
@@ -1,18 +1,25 @@
 package com.music125.stock.parse.financeqq;
 
 
+import com.alibaba.fastjson.JSON;
 import com.arronlong.httpclientutil.HttpClientUtil;
 import com.arronlong.httpclientutil.common.HttpConfig;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.music125.stock.parse.financeqq.response.KchartResponse;
 import com.music125.stock.utils.ParseUtils;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Desc k线图
  */
-//@Slf4j
+@Slf4j
 public class KchartParse {
 
 
@@ -22,27 +29,28 @@ public class KchartParse {
 
     public static void parse(String code){
         try {
+
             url = String.format(url,code);
 
             String jsonResult = HttpClientUtil.get(HttpConfig.custom().url(url));
 
             ObjectMapper mapper = new ObjectMapper();
             JsonNode rootNode = mapper.readTree(jsonResult);
-            List<String> kdata = ParseUtils.getStringList(rootNode, "qfqday");
-
-
+            String kdata = ParseUtils.getValueToString(rootNode, "data","");
+            rootNode = mapper.readTree(kdata);
+            kdata = ParseUtils.getValueToString(rootNode, code,"");
 
             System.out.println(kdata);
 
-//            if(StringUtils.isNoneBlank(kdata)){
-//
-//                if(StringUtils.isNoneBlank(kdata)){
-//
-//                    KchartResponse kcharResult = JSON.parseObject(kdata, KchartResponse.class);
-//
-//                    System.out.println(kcharResult);
-//                }
-//            }
+            if(StringUtils.isNoneBlank(kdata)){
+
+                KchartResponse kcharResult = JSON.parseObject(kdata, KchartResponse.class);
+                if(kcharResult!=null && !CollectionUtils.isEmpty(kcharResult.qfqday)){
+                    for (List<Object> kchartItems:kcharResult.qfqday) {
+                        kchartItems.get(0);
+                    }
+                }
+            }
 
 
         }catch (Exception e){
